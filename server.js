8f3c5d78d0479f1a3751ff61a734630687d2d86e
Emma Ådahl Görling <143135641+EmmaGorling@users.noreply.github.com>
2024-04-19 15:13:44 +0200

    Har lagt till schema för workexperience, nästa steg är att göra routing

--- server.js
+++ server.js
@@ -1,9 +1,50 @@
 const mongoose = require('mongoose');
+const express = require('express');
+const cors = require('cors');
+const { type } = require('os');
 require('dotenv').config();
 
+const app = express();
+const port = process.env.DB_PORT || 3000;
+
+// Connect to MongoDB
 mongoose.connect(process.env.DB_URL).then(() => {
     console.log('Connected to MongoDB');
 }).catch((error) => {
     console.log('Error when trying to connect to MongoDB' + error);
 });
 
+// Workexperience schema
+const WorkexperienceSchema = new mongoose.Schema({
+    companyname: {
+        type: String,
+        required: true
+    },
+    location: {
+        type: String,
+        required: true
+    },
+    jobtitle: {
+        type: String,
+        required: true
+    },
+    startdate: {
+        type: Date,
+        required: true
+    },
+    enddate: {
+        type: Date,
+        required: false
+    },
+    description: {
+        type: String,
+        required: true
+    }
+});
+
+const Workexperience = mongoose.model('Workexperience', WorkexperienceSchema);
+
+
+app.listen(port, () => {
+    console.log('Server is running on port: ' + port);
+});
